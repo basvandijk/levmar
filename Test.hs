e0c87d089c2e18f083a26d72561274e6d312e779
Roel van Dijk <vandijk.roel@gmail.com>
2009-08-20 16:26:48 +0200

    Smart test function in Test.hs
    Ignore-this: bce93e841d01fa32b2f0d2d94a928df7
    
    darcs-hash:20090820142648-b004c-3eb8728084826c4924305f7745e1ee8ad2481b35.gz

--- Test.hs
+++ Test.hs
@@ -1,20 +1,43 @@
 module Main where
 
-import LevMar (dlevmar_dif, defaultOpts)
+import LevMar
+import System.Random
 
-ys :: Fractional a => [a]
-ys = [ 2.90, 3.40, 3.10, 2.79, 3.15
-     , 3.37, 2.87, 3.67, 2.57, 2.78
-     , 2.96, 3.03, 3.34, 2.87, 2.20
-     ]
+model_1 :: [r] -> a -> r
+model_1 [a] _ = a
+model_1 _ _ = error "params"
 
-params :: Num a => [a]
-params = [0]
+model_2 :: Num r => [r] -> r -> r
+model_2 [a, b] x = a*x + b
+model_2 _ _ = error "params"
 
-model :: [a] -> Int -> a
-model [p] _ = p
-model _   _ = error "FOUT!"
+model_3 :: Num r => [r] -> r -> r
+model_3 [a, b, c] x = a*x*x + b*x + c
+model_3 _ _ = error "params"
+
+model_4 :: Num r => [r] -> r -> r
+model_4 [a, b, c, d] x = a*x*x*x + b*x*x + c*x + d
+model_4 _ _ = error "params"
+
+rndGenSeed :: Int
+rndGenSeed = 123456
+
+test :: Show a
+     => Model Double a
+     -> [Double] -- solution
+     -> [a]      -- x-values of samples
+     -> Double   -- noise level
+     -> IO ()
+test f ps xs noise = do let ns = take (length xs) $ randoms $ mkStdGen rndGenSeed
+                        let samples = zip xs $ map (f ps) xs
+                        let samples' = zipWith (\(x, y) n -> (x, y + (n - 0.5) * 2 * noise)) samples ns
+                        result <- dlevmar_dif f
+                                              (replicate (length ps) 0) -- all params 0
+                                              samples'
+                                              1000
+                                              defaultOpts
+                        print result
+
 
 main :: IO ()
-main = do result <- dlevmar_dif model params (zip [0..] ys) 1000 defaultOpts
-          print result
+main = test model_3 [5, 3, 7] [1..50] 2
