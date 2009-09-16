8038f737ce159a1119f4fcd06ede84809a5e99c5
Bas van Dijk <v.dijk.bas@gmail.com>
2009-09-16 10:24:34 +0200

    Added foldr and foldrN to SizedList and wrote some functions in terms of them
    Ignore-this: b178c37a42cf259c5ac8173fc16f27d3
    
    darcs-hash:20090916082434-4237f-e4dbe90a84e3541e7deddf5538970ddce7a4c4ce.gz

--- SizedList.hs
+++ SizedList.hs
@@ -1,17 +1,20 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
 
 module SizedList
     ( SizedList(..)
+    , foldr
+    , foldrN
     , toList
+    , length
     , fromList
     , unsafeFromList
-    , length
     , replicate
     ) where
 
 
-import Prelude hiding ( replicate, length )
+import Prelude hiding ( foldr, replicate, length )
 import Data.Maybe     ( fromMaybe )
 import TypeLevelNat   ( Z(..), S(..), Nat, induction, witnessNat, N(..) )
 
@@ -46,24 +49,30 @@ instance Show a => Show (SizedList n a) where
 
 --------------------------------------------------------------------------------
 
-newtype ToList a n = TL { unTL :: SizedList n a -> [a] }
+-- | Fold a binary operator over a @SizedList@.
+foldr :: forall a b n. (a -> b -> b) -> b -> SizedList n a -> b
+foldr f z = foldr_f_z
+    where
+      foldr_f_z :: forall k. SizedList k a -> b
+      foldr_f_z Nil        = z
+      foldr_f_z (x ::: xs) = f x $ foldr_f_z xs
+
+-- | Fold a binary operator yielding a value with a natural number
+-- indexed type over a @SizedList@.
+foldrN :: forall a b n. (forall m. a -> b m -> b (S m)) -> b Z -> SizedList n a -> b n
+foldrN f z = foldrN_f_z
+    where
+      foldrN_f_z :: forall k. SizedList k a -> b k
+      foldrN_f_z Nil        = z
+      foldrN_f_z (x ::: xs) = f x $ foldrN_f_z xs
 
 -- | Convert a @SizedList@ to a normal list.
-toList :: forall a n. Nat n => SizedList n a -> [a]
-toList = unTL $ induction (witnessNat :: n)
-                          (TL tlZ)
-                          (TL . tlS . unTL)
-    where
-      tlZ :: SizedList Z a -> [a]
-      tlZ Nil = []
-      tlZ _   = canNeverHappen
+toList :: SizedList n a -> [a]
+toList = foldr (:) []
 
-      tlS :: forall x. Nat x => (SizedList x a -> [a]) -> SizedList (S x) a -> [a]
-      tlS f (x ::: xs) = x : f xs
-      tlS _ _          = canNeverHappen
-
-canNeverHappen :: error
-canNeverHappen = error "SizedList.toList: can never happen!"
+-- | Returns the length of the @SizedList@.
+length :: SizedList n a -> N n
+length = foldrN (const Succ) Zero
 
 
 --------------------------------------------------------------------------------
@@ -73,9 +82,7 @@ newtype FromList a n = FL { unFL :: [a] -> Maybe (SizedList n a) }
 -- | Convert a normal list to a @SizedList@. If the length of the given
 -- list does not equal @n@, @Nothing@ is returned.
 fromList :: forall a n. Nat n => [a] -> Maybe (SizedList n a)
-fromList = unFL $ induction (witnessNat :: n)
-                            (FL flZ)
-                            (FL . flS . unFL)
+fromList = unFL $ induction (witnessNat :: n) (FL flZ) (FL . flS . unFL)
     where
       flZ [] = Just Nil
       flZ _  = Nothing
@@ -96,16 +103,7 @@ newtype Replicate a n = R { unR :: SizedList n a}
 
 -- | @replicate x :: SizedList n a@ returns a @SizedList@ of @n@ @x@s.
 replicate :: forall a n. Nat n => a -> SizedList n a
-replicate x = unR $ induction (witnessNat :: n)
-                              (R Nil)
-                              (R . (x :::) . unR)
-
---------------------------------------------------------------------------------
-
--- | Returns the length of the @SizedList@.
-length :: SizedList n a -> N n
-length Nil        = Zero
-length (_ ::: xs) = Succ $ length xs
+replicate x = unR $ induction (witnessNat :: n) (R Nil) (R . (x :::) . unR)
 
 
 -- The End ---------------------------------------------------------------------
