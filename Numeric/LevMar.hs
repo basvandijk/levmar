b4ee340bbded2417651f2c4fbba9770862793d13
Bas van Dijk <v.dijk.bas@gmail.com>
2011-04-11 08:10:57 +0200

    Internal documentation
    Ignore-this: f70abf93ab92adbe9159819a4875bed9
    
    darcs-hash:20110411061057-ae560-c217ff9d9d21fe26f6be7b0537d1dfe33531acba.gz

--- Numeric/LevMar.hs
+++ Numeric/LevMar.hs
@@ -50,35 +50,28 @@ module Numeric.LevMar
 -------------------------------------------------------------------------------
 
 -- from base:
-import Control.Monad.Instances -- for 'instance Functor (Either a)'
-import Control.Monad         ( mplus )
+import Control.Monad         ( return, mplus )
 import Control.Exception     ( Exception )
 import Data.Typeable         ( Typeable )
 import Data.Either           ( Either(Left, Right) )
 import Data.Function         ( ($) )
+import Data.Functor          ( (<$>) )
 import Data.Int              ( Int )
-import Data.List             ( lookup )
-import Data.Maybe            ( Maybe(Nothing, Just)
-                             , isJust, fromJust, fromMaybe
-                             )
+import Data.List             ( lookup, (++) )
+import Data.Maybe            ( Maybe(Nothing, Just), isJust, fromJust, fromMaybe )
 import Data.Monoid           ( Monoid(mempty, mappend) )
 import Data.Ord              ( (<) )
-import Foreign.Marshal.Array ( allocaArray, withArray
-                             , peekArray, copyArray
-                             )
+import Foreign.Marshal.Array ( allocaArray, withArray, peekArray, copyArray )
 import Foreign.Ptr           ( Ptr, nullPtr )
-import Foreign.ForeignPtr    ( newForeignPtr_, mallocForeignPtrArray
-                             , withForeignPtr
-                             )
+import Foreign.ForeignPtr    ( newForeignPtr_, mallocForeignPtrArray, withForeignPtr )
 import Foreign.Storable      ( Storable )
-import Prelude               ( Enum, Fractional, RealFrac
-                             , Float, Double
-                             , toEnum, (-), error, floor
+import Prelude               ( Enum, Fractional, RealFrac, Float, Double
+                             , toEnum, (-), (*), error, floor
                              )
 import System.IO             ( IO )
 import System.IO.Unsafe      ( unsafePerformIO )
 import Text.Read             ( Read )
-import Text.Show             ( Show )
+import Text.Show             ( Show, show )
 
 #if __GLASGOW_HASKELL__ < 700
 import Prelude               ( fromInteger )
@@ -88,7 +81,6 @@ import Prelude               ( fromInteger )
 import Data.Bool.Unicode     ( (∧), (∨) )
 import Data.Eq.Unicode       ( (≢) )
 import Data.Function.Unicode ( (∘) )
-import Prelude.Unicode       ( (⋅) )
 
 -- from hmatrix:
 import Data.Packed.Vector ( Vector )
@@ -249,118 +241,152 @@ gen_levmar f_der
            f_lec_dif
            f_blec_der
            f_blec_dif
-           model mJac ps ys itMax opts (Constraints mLowBs mUpBs mWeights mLinC)
-    = unsafePerformIO $ do
+           model mJac ps ys itMax opts (Constraints mLowBs mUpBs mWeights mLinC) =
+  -- All effects are contained, so we can safely perform:
+  unsafePerformIO $ do
 
-        -- Allocate space for the parameters array and copy the contents of the
-        -- given parameters vector to it. Note that we can't pass the given
-        -- parameters vector to the C levmar function because the array will be
-        -- modified during execution which will violate referential
-        -- transparency. We create a ForeignPtr because the parameter array is
-        -- later turned into a storable Vector.
-        psFP ← mallocForeignPtrArray lenPs
-        withForeignPtr psFP $ \psPtr → do
-          VS.unsafeWith ps $ \psPtrInp →
-            copyArray psPtr psPtrInp lenPs
+    -- We need to pass the initial parameters 'ps' to the C function.
+    -- However, we can't just pass a pointer to them because the C function 
+    -- will modify the parameters during execution which will violate 
+    -- referential transparanency. Instead we allocate new space 
+    -- and copy the parameters to it.
+    --
+    -- Note that, in the end, the array is returned from this function.
+    -- This means that the only way to guarantee its finalisation
+    -- is to allocate it using a ForeignPtr:
+    psFP ← mallocForeignPtrArray lenPs
+    withForeignPtr psFP $ \psPtr → do
+      VS.unsafeWith ps $ \psPtrInp →
+        copyArray psPtr psPtrInp lenPs
 
-          VS.unsafeWith ys $ \ysPtr →
-            withArray (optsToList opts) $ \optsPtr →
-              allocaArray c'LM_INFO_SZ $ \infoPtr → do
+      -- Retrieve the (read-only) pointer 'ysPtr' to the samples vector 'ys'
+      -- so we can pass it to the C function:
+      VS.unsafeWith ys $ \ysPtr →
 
-                covarFP ← mallocForeignPtrArray covarLen
-                withForeignPtr covarFP $ \covarPtr →
+        -- Convert the Options 'opts' to a list and then to an array
+        -- so we can pass the (read-only) pointer 'optsPtr' to the C function:
+        withArray (optsToList opts) $ \optsPtr →
 
-                  let cmodel ∷ Bindings.LevMar.Model r
-                      cmodel parPtr hxPtr _ _ _ = do
-                        parFP ← newForeignPtr_ parPtr
-                        let psV = VS.unsafeFromForeignPtr parFP 0 lenPs
-                            vector = model psV
-                        VS.unsafeWith vector $ \p → copyArray hxPtr p (VS.length vector)
-                  in withModel cmodel $ \modelPtr → do
+          -- Allocate space for the info array
+          -- so we can pass it to the C function.
+          -- Note that, in the end, this array is converted to an Info value
+          -- and returned from this function.
+          allocaArray c'LM_INFO_SZ $ \infoPtr → do
 
-                     -- Calling the correct low-level levmar function:
-                     let runDif ∷ LevMarDif r → IO Int
-                         runDif f = f modelPtr
-                                      psPtr
-                                      ysPtr
-                                      lenPs
-                                      lenYs
-                                      itMax
-                                      optsPtr
-                                      infoPtr
-                                      nullPtr
-                                      covarPtr
-                                      nullPtr
+            -- Allocate space for the covariance matrix
+            -- so we can pass it to the C function.
+            -- Like the parameters array the matrix
+            -- needs to be returned from this function.
+            -- So we also allocate it using a ForeignPtr:
+            covarFP ← mallocForeignPtrArray covarLen
+            withForeignPtr covarFP $ \covarPtr →
 
-                     err ← case mJac of
-                       Nothing → if boxConstrained
-                                 then if linConstrained
-                                      then withBoxConstraints
-                                               (withLinConstraints $ withWeights runDif)
-                                               f_blec_dif
-                                      else withBoxConstraints runDif f_bc_dif
-                                 else if linConstrained
-                                      then withLinConstraints runDif f_lec_dif
-                                      else runDif f_dif
-                       Just jac →
-                         let cjacobian ∷ Bindings.LevMar.Jacobian r
-                             cjacobian parPtr jPtr _ _ _ = do
-                               parFP ← newForeignPtr_ parPtr
-                               let psV    = VS.unsafeFromForeignPtr parFP 0 lenPs
-                                   matrix = jac psV
-                                   vector = flatten matrix
-                               VS.unsafeWith vector $ \p →
-                                 copyArray jPtr p (VS.length vector)
-                         in withJacobian cjacobian $ \jacobPtr →
-                           let runDer ∷ LevMarDer r → IO Int
-                               runDer f = runDif $ f jacobPtr
-                           in if boxConstrained
-                              then if linConstrained
-                                   then withBoxConstraints
-                                            (withLinConstraints $ withWeights runDer)
-                                            f_blec_der
-                                   else withBoxConstraints runDer f_bc_der
-                              else if linConstrained
-                                   then withLinConstraints runDer f_lec_der
-                                   else runDer f_der
+              -- 'cmodel' is the low-level model function which is converted
+              -- to the FunPtr 'modelFunPtr' and passed to the C function.
+              -- 'cmodel' will first convert the parameters pointer 'parPtr'
+              -- into a Vector after converting it into a ForeignPtr
+              -- (without a finalizer).
+              -- Then it will apply the high-level 'model' function
+              -- to this parameter vector. The resulting vector is then copied
+              -- to the output buffer 'hxPtr':
+              let cmodel ∷ Bindings.LevMar.Model r
+                  cmodel parPtr hxPtr _ _ _ = do
+                    parFP ← newForeignPtr_ parPtr
+                    let psV = VS.unsafeFromForeignPtr parFP 0 lenPs
+                        vector = model psV
+                    VS.unsafeWith vector $ \p → copyArray hxPtr p (VS.length vector)
+              in withModel cmodel $ \modelFunPtr → do
 
-                     -- Handling errors:
-                     if err < 0
-                        -- we don't treat these two as an error
-                        ∧ err ≢ c'LM_ERROR_SINGULAR_MATRIX
-                        ∧ err ≢ c'LM_ERROR_SUM_OF_SQUARES_NOT_FINITE
-                       then return $ Left $ convertLevMarError err
-                       else -- Converting results:
-                            do info ← peekArray c'LM_INFO_SZ infoPtr
-                               return $ Right
-                                 ( VS.unsafeFromForeignPtr psFP 0 lenPs
-                                 , listToInfo info
-                                 , reshape lenPs $
-                                     VS.unsafeFromForeignPtr covarFP 0 covarLen
-                                 )
-      where
-        lenPs          = VS.length ps
-        lenYs          = VS.length ys
-        covarLen       = lenPs⋅lenPs
-        (cMat, rhcVec) = fromJust mLinC
+                 -- All the low-level C functions share a common set of arguments.
+                 -- 'runDif' applies these arguments to the given C function 'f':
+                 let runDif ∷ LevMarDif r → IO Int
+                     runDif f = f modelFunPtr
+                                  psPtr
+                                  ysPtr
+                                  lenPs
+                                  lenYs
+                                  itMax
+                                  optsPtr
+                                  infoPtr
+                                  nullPtr
+                                  covarPtr
+                                  nullPtr
 
-        -- Whether the parameters are constrained by a linear equation.
-        linConstrained = isJust mLinC
+                 err ← case mJac of
+                   Nothing → if boxConstrained
+                             then if linConstrained
+                                  then withBoxConstraints
+                                         (withLinConstraints $ withWeights runDif)
+                                         f_blec_dif
+                                  else withBoxConstraints runDif f_bc_dif
+                             else if linConstrained
+                                  then withLinConstraints runDif f_lec_dif
+                                  else runDif f_dif
 
-        -- Whether the parameters are constrained by a bounding box.
-        boxConstrained = isJust mLowBs ∨ isJust mUpBs
+                   Just jac →
+                     let cjacobian ∷ Bindings.LevMar.Jacobian r
+                         cjacobian parPtr jPtr _ _ _ = do
+                           parFP ← newForeignPtr_ parPtr
+                           let psV    = VS.unsafeFromForeignPtr parFP 0 lenPs
+                               matrix = jac psV
+                               vector = flatten matrix
+                           VS.unsafeWith vector $ \p →
+                             copyArray jPtr p (VS.length vector)
+                     in withJacobian cjacobian $ \jacobPtr →
 
-        withBoxConstraints f g =
-            maybeWithArray mLowBs $ \lBsPtr →
-              maybeWithArray mUpBs $ \uBsPtr →
-                f $ g lBsPtr uBsPtr
+                       let runDer ∷ LevMarDer r → IO Int
+                           runDer f = runDif $ f jacobPtr
+                       in if boxConstrained
+                          then if linConstrained
+                               then withBoxConstraints
+                                      (withLinConstraints $ withWeights runDer)
+                                      f_blec_der
+                               else withBoxConstraints runDer f_bc_der
+                          else if linConstrained
+                               then withLinConstraints runDer f_lec_der
+                               else runDer f_der
 
-        withLinConstraints f g =
-            VS.unsafeWith (flatten cMat) $ \cMatPtr →
-              VS.unsafeWith rhcVec $ \rhcVecPtr →
-                f ∘ g cMatPtr rhcVecPtr $ rows cMat
+                 -- Handling errors:
+                 if err < 0
+                    -- we don't treat the following two as an error:
+                    ∧ err ≢ c'LM_ERROR_SINGULAR_MATRIX
+                    ∧ err ≢ c'LM_ERROR_SUM_OF_SQUARES_NOT_FINITE
+                   then return $ Left $ convertLevMarError err
 
-        withWeights f g = maybeWithArray mWeights $ f ∘ g
+                   else do -- Converting results:
+                           info ← listToInfo <$> peekArray c'LM_INFO_SZ infoPtr
+                           let psV = VS.unsafeFromForeignPtr psFP 0 lenPs
+                           let covarM = reshape lenPs $
+                                          VS.unsafeFromForeignPtr covarFP 0 covarLen
+
+                           return $ Right (psV, info, covarM)
+  where
+    lenPs          = VS.length ps
+    lenYs          = VS.length ys
+    covarLen       = lenPs*lenPs
+    (cMat, rhcVec) = fromJust mLinC
+
+    -- Whether the parameters are constrained by a linear equation.
+    linConstrained = isJust mLinC
+
+    -- Whether the parameters are constrained by a bounding box.
+    boxConstrained = isJust mLowBs ∨ isJust mUpBs
+
+    withBoxConstraints f g =
+        maybeWithArray mLowBs $ \lBsPtr →
+          maybeWithArray mUpBs $ \uBsPtr →
+            f $ g lBsPtr uBsPtr
+
+    withLinConstraints f g =
+        VS.unsafeWith (flatten cMat) $ \cMatPtr →
+          VS.unsafeWith rhcVec $ \rhcVecPtr →
+            f ∘ g cMatPtr rhcVecPtr $ rows cMat
+
+    withWeights f g = maybeWithArray mWeights $ f ∘ g
+
+maybeWithArray ∷ (Storable α) ⇒ Maybe (Vector α) → (Ptr α → IO β) → IO β
+maybeWithArray Nothing  f = f nullPtr
+maybeWithArray (Just v) f = VS.unsafeWith v f
 
 
 --------------------------------------------------------------------------------
@@ -407,25 +433,22 @@ data Constraints r = Constraints
     , linearConstraints ∷ Maybe (LinearConstraints r) -- ^ Optional linear constraints
     }
 
--- | * 'mempty' is defined as a 'Constraints' where all fields are 'Nothing'.
---
--- * 'mappend' merges two 'Constraints' by taking the first non-'Nothing' value for each field.
-instance Monoid (Constraints r) where
-    mempty = Constraints Nothing Nothing Nothing Nothing
-    mappend (Constraints lb1 ub1 w1 l1)
-            (Constraints lb2 ub2 w2 l2) = Constraints (mplus lb1 lb2)
-                                                      (mplus ub1 ub2)
-                                                      (mplus w1 w2)
-                                                      (mplus l1 l2)
-
 -- | Linear constraints consisting of a constraints matrix, @k><m@ and
 --   a right hand constraints vector, of length @k@ where @m@ is the number of
 --   parameters and @k@ is the number of constraints.
 type LinearConstraints r = (Matrix r, Vector r)
 
-maybeWithArray ∷ (Storable α) ⇒ Maybe (Vector α) → (Ptr α → IO β) → IO β
-maybeWithArray Nothing  f = f nullPtr
-maybeWithArray (Just v) f = VS.unsafeWith v f
+-- | * 'mempty' is defined as a 'Constraints' where all fields are 'Nothing'.
+--
+--   * 'mappend' merges two 'Constraints' by taking the first non-'Nothing' value
+--     for each field.
+instance Monoid (Constraints r) where
+    mempty = Constraints Nothing Nothing Nothing Nothing
+    mappend (Constraints lb1 ub1 w1 l1)
+            (Constraints lb2 ub2 w2 l2) = Constraints (lb1 `mplus` lb2)
+                                                      (ub1 `mplus` ub2)
+                                                      (w1  `mplus` w2)
+                                                      (l1  `mplus` l2)
 
 
 --------------------------------------------------------------------------------
@@ -524,8 +547,8 @@ levmarCErrorToLevMarError =
     ]
 
 convertLevMarError ∷ Int → LevMarError
-convertLevMarError err = fromMaybe (error "Unknown levmar error") $
-                         lookup err levmarCErrorToLevMarError
+convertLevMarError err = fromMaybe (error $ "Unknown levmar error: " ++ show err)
+                                   (lookup err levmarCErrorToLevMarError)
 
 
 -- The End ---------------------------------------------------------------------
