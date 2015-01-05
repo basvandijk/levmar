b732b877a297c67df90d3f7699a77ce316c035f1
Bas van Dijk <v.dijk.bas@gmail.com>
2015-01-05 07:46:20 +0100

    Add levmar.nix and default.nix

---
+++ levmar.nix
@@ -0,0 +1,16 @@
+{ cabal, baseUnicodeSymbols, bindingsLevmar, hmatrix, vector }:
+
+cabal.mkDerivation (self: {
+  pname = "levmar";
+  version = "HEAD";
+  src = ./.;
+  buildDepends = [
+    baseUnicodeSymbols bindingsLevmar hmatrix vector
+  ];
+  meta = {
+    homepage = "https://github.com/basvandijk/levmar";
+    description = "An implementation of the Levenberg-Marquardt algorithm";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
