32ebdb62b442b433582903b2c8f6f537b7e43e9d
Bas van Dijk <v.dijk.bas@gmail.com>
2016-07-17 20:37:43 +0200

    Updated Nix files

--- levmar.nix
+++ levmar.nix
@@ -1,14 +1,10 @@
-{ cabal, bindingsLevmar, hmatrix, vector }:
-
-cabal.mkDerivation (self: {
+{ mkDerivation, base, bindings-levmar, hmatrix, stdenv, vector }:
+mkDerivation {
   pname = "levmar";
   version = "HEAD";
   src = ./.;
-  buildDepends = [ bindingsLevmar hmatrix vector ];
-  meta = {
-    homepage = "https://github.com/basvandijk/levmar";
-    description = "An implementation of the Levenberg-Marquardt algorithm";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  libraryHaskellDepends = [ base bindings-levmar hmatrix vector ];
+  homepage = "https://github.com/basvandijk/levmar";
+  description = "An implementation of the Levenberg-Marquardt algorithm";
+  license = stdenv.lib.licenses.bsd3;
+}
