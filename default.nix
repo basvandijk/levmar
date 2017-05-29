87b21b378b134c7786a19385e79271e28bab2952
Bas van Dijk <v.dijk.bas@gmail.com>
2017-05-29 23:05:34 +0200

    Updated Nix files

--- default.nix
+++ default.nix
@@ -1,4 +1,17 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.haskellPackages.callPackage ./levmar.nix {
-     bindingsLevmar = import ../bindings-levmar;
-   }
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage (import ./levmar.nix) {
+    bindings-levmar = haskellPackages.callPackage (import ../bindings-levmar/bindings-levmar.nix) {};
+  };
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
